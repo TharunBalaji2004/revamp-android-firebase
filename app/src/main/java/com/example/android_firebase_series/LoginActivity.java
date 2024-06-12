85cb0c11cd8429e43374c905d53c31e85259b696
Tharun Balaji R <tharunbalaji31@gmail.com>
2024-06-12 21:55:51 +0530

    Day 1: Firebase Authentication

---
+++ app/src/main/java/com/example/android_firebase_series/LoginActivity.java
@@ -0,0 +1,68 @@
+package com.example.android_firebase_series;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                firebaseLogin(email, password);
+            }
+        });
+    }
+
+    private void firebaseLogin(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("FIREBASE", "Login successful");
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finishAffinity();
+                        } else {
+                            Log.d("FIREBASE", "Login error occurred: "+task.getException());
+
+                            Toast.makeText(LoginActivity.this, "Signup Error occurred", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
+
---
+++ app/src/main/java/com/example/android_firebase_series/SignupActivity.java
@@ -0,0 +1,91 @@
+package com.example.android_firebase_series;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignupActivity extends AppCompatActivity {
+    private EditText etEmail, etPassword;
+    private Button btnSignup, btnLogin;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            Intent intent = new Intent(SignupActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        btnSignup = findViewById(R.id.btn_signup);
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                firebaseSignup(email, password);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void firebaseSignup(String email, String password) {
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password) // firebase server
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("FIREBASE", "User has been created");
+                            Intent intent = new Intent(SignupActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finishAffinity();
+                        } else {
+                            Log.d("FIREBASE", "Signup error occurred: "+task.getException());
+
+                            Toast.makeText(SignupActivity.this, "Signup Error occurred", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
+
+// 1. Signup Page
+// 2. Login Page
+// 3. Home Page
+
